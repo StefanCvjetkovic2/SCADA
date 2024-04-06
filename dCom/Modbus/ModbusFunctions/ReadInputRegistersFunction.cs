372712bda1c2112f977d74d978a616a00eba4be7
StefanCvjetkovic2 <cvjetkovic.pr34.2021@uns.ac.rs>
2024-04-06 22:18:44 +0200

    Read funkcije

--- dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadInputRegistersFunction.cs
@@ -24,15 +24,48 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            byte[] PackReq = new byte[12];
+
+            PackReq[0] = BitConverter.GetBytes(CommandParameters.TransactionId)[1];
+            PackReq[1] = BitConverter.GetBytes(CommandParameters.TransactionId)[0];
+            PackReq[2] = BitConverter.GetBytes(CommandParameters.ProtocolId)[1];
+            PackReq[3] = BitConverter.GetBytes(CommandParameters.ProtocolId)[0];
+            PackReq[4] = BitConverter.GetBytes(CommandParameters.Length)[1];
+            PackReq[5] = BitConverter.GetBytes(CommandParameters.Length)[0];
+            PackReq[6] = CommandParameters.UnitId;
+            PackReq[7] = CommandParameters.FunctionCode;
+            PackReq[8] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).StartAddress)[1];
+            PackReq[9] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).StartAddress)[0];
+            PackReq[10] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).Quantity)[1];
+            PackReq[11] = BitConverter.GetBytes(((ModbusReadCommandParameters)CommandParameters).Quantity)[0];
+
+            return PackReq;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> resp = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            int byteCount = response[8];
+            ushort startAddress = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+
+            if (response[7] == CommandParameters.FunctionCode + 0x80)
+            {
+                HandeException(response[8]);
+            }
+            else
+            {
+                for (int i = 0; i < byteCount; i += 2)
+                {
+                    ushort value = BitConverter.ToUInt16(response, 9 + i);
+                    value = (ushort)IPAddress.NetworkToHostOrder((short)value);
+                    Tuple<PointType, ushort> tuple = new Tuple<PointType, ushort>(PointType.ANALOG_INPUT, startAddress++);
+                    resp.Add(tuple, value);
+                }
+            }
+
+            return resp;
         }
     }
 }
